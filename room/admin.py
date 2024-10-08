062dac5e73cf650cabccfab4e1f122329eb0016f
Ei Sin <einge174@gmail.com>
2024-10-08 12:16:37 +0000

    create two models RoomPost and Comments.Also add urls.py in roomapp and update admin.py

--- room/admin.py
+++ room/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
+from .models import RoomPost,Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+@admin.register(RoomPost)
+
+class RoomPostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title','created_on','status')
+    summernote_fields = ('description',)
