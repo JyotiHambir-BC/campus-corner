9d9b9efa44f4fef6b2c0c22a121127b753ea8cad
Jyoti Hambir <qdde3t1v@students.codeinstitute.net>
2024-10-09 09:27:40 +0000

    changed allowed hosts in settings.py .codeinstitute-ide.net

--- student_marketplace/settings.py
+++ student_marketplace/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '.herokuapp.com']
+ALLOWED_HOSTS = ['.codeinstitute-ide.net', '.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = ["https://*.codeinstitute-ide.net", "https://*.herokuapp.com"]
 
